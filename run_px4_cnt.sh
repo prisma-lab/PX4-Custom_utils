f08e2002d19270224a0782a7c2c030c0fd9e2a7f
fp018 <pagano.francesca2@gmail.com>
2024-07-13 21:50:12 +0200

    removed submodule

--- run_px4_cnt.sh
+++ run_px4_cnt.sh
@@ -9,7 +9,7 @@ xhost +
 docker run -it --rm --privileged \
 -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 -v "/dev:/dev" \
---mount type=bind,source=$(pwd)/../PX4-orbit,target=/root/PX4-orbit \
+--mount type=bind,source=$(pwd)/PX4-orbit,target=/root/PX4-orbit \
 --mount type=bind,source=$(pwd)/ros2_ws,target=/root/ros2_ws/src/pkg \
 --env="DISPLAY=$DISPLAY" \
 --network host \
