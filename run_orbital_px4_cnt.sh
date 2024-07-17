c722149ba487f810b5be5cd30319518e1563f0f7
fp018 <86669078+fp018@users.noreply.github.com>
2024-07-17 15:35:52 +0200

    Update run_orbital_px4_cnt.sh

--- run_orbital_px4_cnt.sh
+++ run_orbital_px4_cnt.sh
@@ -7,7 +7,7 @@ xhost +
 docker run -it --rm --privileged \
 -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 -v "/dev:/dev" \
---mount type=bind,source=$(pwd)/PX4_orbital,target=/rootPX4_orbital \
+--mount type=bind,source=$(pwd)/PX4_orbital,target=/root/PX4_orbital \
 --mount type=bind,source=$(pwd)/ros2_ws,target=/root/ros2_ws/src/pkg \
 --env="DISPLAY=$DISPLAY" \
 --network host \
